9bb416a03d3ff20cb7660e8e48b06ca3ba45064d
Hans Bonini <hans@ysolutions.com.br>
2021-12-01 20:27:58 -0300

    Embedding urctbase.dll inside .exe

--- build_windows.sh
+++ build_windows.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 PYINSTALLER_PATH="/home/hans/.wine/drive_c/users/hans/Local Settings/Application Data/Programs/Python/Python310-32/Scripts/"
-wine "$PYINSTALLER_PATH/pyinstaller.exe" --onefile main.py
+wine "$PYINSTALLER_PATH/pyinstaller.exe" --add-data "dll/ucrtbase.dll;." --onefile main.py
