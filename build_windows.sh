413ca15295fec91bbc6bb3ffc9c97d75c89d17e3
Hans Bonini <hans@ysolutions.com.br>
2021-12-02 00:41:33 -0300

    Fixed Windows Executable

--- build_windows.sh
+++ build_windows.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-PYINSTALLER_PATH="/home/hans/.wine/drive_c/users/hans/Local Settings/Application Data/Programs/Python/Python310-32/Scripts/"
-wine "$PYINSTALLER_PATH/pyinstaller.exe" --add-data "dll/ucrtbase.dll;." --onefile main.py
+PYINSTALLER_PATH="/home/hans/.wine/drive_c/Python35/Scripts"
+wine "$PYINSTALLER_PATH/pyinstaller.exe" --onefile main.py
